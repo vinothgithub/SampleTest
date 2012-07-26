0c78e734e3c99a6028e478b1fc0d81103e4c22d2
vinothgithub <vinothks@softcrylic.co.in>
2012-07-26 22:52:21 +0530

    first commit

---
+++ src/test/SampleTest.java
@@ -0,0 +1,57 @@
+package test;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
+@Test
+public class SampleTest 
+{
+
+	@BeforeSuite(alwaysRun = true)
+    @Parameters({ "requestId", "reportPath", "Environment", "ExecutionType",
+                  "ApplicationToTest" }) 
+	public void beforeFixture()
+	{
+		
+	}
+
+	@Parameters({ "browser", "browserVersion", "os", "dataSource" })
+	@BeforeClass
+	public void beforeClassTest()
+	{
+		
+	}
+	
+	@Test
+	public void doTest()
+	{
+		WebDriver driver = new FirefoxDriver();
+		driver.navigate().to("http://www.softcrylic.com");
+		driver.findElement(By.id("solutions-button")).click();
+		driver.findElement(By.linkText("Microsoft .NET Development")).click();
+		driver.findElement(By.linkText("E-commerce Development")).click();
+		driver.findElement(By.id("li4")).findElement(By.tagName("a")).click();
+		driver.findElement(By.className("partner_logo")).findElement(By.tagName("a")).click();
+		
+		driver.quit();
+	}
+	@Test
+	public void doTestChrome()
+	{
+		WebDriver driver = new ChromeDriver();
+		driver.navigate().to("http://www.softcrylic.com");
+		driver.findElement(By.id("solutions-button")).click();
+		driver.findElement(By.linkText("Microsoft .NET Development")).click();
+		driver.findElement(By.linkText("E-commerce Development")).click();
+		driver.findElement(By.id("li4")).findElement(By.tagName("a")).click();
+		driver.findElement(By.className("partner_logo")).findElement(By.tagName("a")).click();
+		
+		driver.quit();
+	}
+}
