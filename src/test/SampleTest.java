b82dcf147fc57b52fa2836cbf13493d2f8943de9
vinothgithub <vinothks@softcrylic.co.in>
2012-07-27 02:37:43 +0530

    ddsfsdf

--- src/test/SampleTest.java
+++ src/test/SampleTest.java
@@ -2,7 +2,6 @@ package test;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeSuite;
